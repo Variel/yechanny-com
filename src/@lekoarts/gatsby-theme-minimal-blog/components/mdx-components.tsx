0a11d4700e083cf1a299913c6f614da9d3d81e85
이예찬 <blackmagic2@naver.com>
2023-01-04 22:49:27 +0900

    트윗 임베드 정렬 변경

--- src/@lekoarts/gatsby-theme-minimal-blog/components/mdx-components.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/mdx-components.tsx
@@ -1,9 +1,11 @@
+/** @jsx jsx */
 import React from "react";
+import { Box, Container, jsx, get } from "theme-ui"
 import MdxComponents from "@lekoarts/gatsby-theme-minimal-blog/src/components/mdx-components";
 import { TwitterTweetEmbed } from "react-twitter-embed";
 
 const AdditionalMdxComponents = {
-  TwitterTweetEmbed: (props: any) => <TwitterTweetEmbed {...props} />,
+  TwitterTweetEmbed: (props: any) => <Box sx={{display: 'flex', justifyContent: 'center'}}><TwitterTweetEmbed {...props} /></Box>,
   ...MdxComponents
 };
 
