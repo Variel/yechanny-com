6b1583e00c9186c401052825abeafff39e18e99c
이예찬 <blackmagic2@naver.com>
2023-01-06 00:29:36 +0900

    테마 변경

--- src/@lekoarts/gatsby-theme-minimal-blog/gatsby-plugin-theme-ui/index.js
+++ src/@lekoarts/gatsby-theme-minimal-blog/gatsby-plugin-theme-ui/index.js
@@ -5,9 +5,12 @@ const theme = merge(parentTheme, {
   fonts:  {
     body: `"Pretendard Variable", -apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"`,
   },
-  styles: {
-    root: {
-      fontSize: '18px'
+  fontSizes: [
+    '14px', '16px', '18px', '20px', '24px', '32px', '48px', '64px', '72px'
+  ],
+  layout: {
+    container: {
+      maxWidth: `920px`
     }
   }
 })
