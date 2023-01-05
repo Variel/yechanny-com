f472587a242d644e09460ca8ac5ebe66f4122eb0
이예찬 <blackmagic2@naver.com>
2023-01-06 00:36:12 +0900

    모바일에서 폰트 크기 변경

--- src/@lekoarts/gatsby-theme-minimal-blog/gatsby-plugin-theme-ui/index.js
+++ src/@lekoarts/gatsby-theme-minimal-blog/gatsby-plugin-theme-ui/index.js
@@ -1,18 +1,31 @@
-import { merge } from "theme-ui"
+import { merge } from "theme-ui";
 import parentTheme from "@lekoarts/gatsby-theme-minimal-blog/src/gatsby-plugin-theme-ui/index";
 
 const theme = merge(parentTheme, {
-  fonts:  {
+  fonts: {
     body: `"Pretendard Variable", -apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"`,
   },
   fontSizes: [
-    '14px', '16px', '18px', '20px', '24px', '32px', '48px', '64px', '72px'
+    "14px",
+    "16px",
+    "18px",
+    "20px",
+    "24px",
+    "32px",
+    "48px",
+    "64px",
+    "72px",
   ],
+  styles: {
+    p: {
+      fontSize: [2, 2, 2, 3],
+    },
+  },
   layout: {
     container: {
-      maxWidth: `920px`
-    }
-  }
-})
+      maxWidth: `920px`,
+    },
+  },
+});
 
 export default theme;
